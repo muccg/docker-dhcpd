be1d5f56d79e7799821981d3ef75cb391abcaa96
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2015-05-06 11:34:02 +0800

    eth0

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@ if [ "$1" = 'dhcpd' ]; then
     if [ ! -f /data/dhcpd.leases ]; then
         touch /data/dhcpd.leases
     fi
-    /usr/sbin/dhcpd -f -cf /data/dhcpd.conf -lf /data/dhcpd.leases --no-pid 2>&1 | tee /data/dhcpd.log
+    /usr/sbin/dhcpd -f -cf /data/dhcpd.conf -lf /data/dhcpd.leases --no-pid eth0 2>&1 | tee /data/dhcpd.log
     exit $?
 fi
 
